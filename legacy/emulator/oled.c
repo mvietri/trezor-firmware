f7e987f045aa93e9ac0fde54792c95ec6a12282f
Mauro Vietri <github@mvietri.dev>
2020-09-02 21:14:40 -0300

    Merge tag 'legacy/v1.9.3' into pitrezor

--- legacy/emulator/oled.c
+++ legacy/emulator/oled.c
@@ -103,9 +103,6 @@ void oledInit(void) {
   texture =
       SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
                         SDL_TEXTUREACCESS_STREAMING, OLED_WIDTH, OLED_HEIGHT);
-#ifdef PIZERO
-  pizeroInit();	
-#endif
 
   oledClear();
   oledRefresh();
@@ -132,9 +129,6 @@ void oledRefresh(void) {
   SDL_UpdateTexture(texture, NULL, data, OLED_WIDTH * sizeof(uint32_t));
   SDL_RenderCopy(renderer, texture, NULL, &dstrect);
   SDL_RenderPresent(renderer);
-#ifdef PIZERO
-  pizeroRefresh(buffer);
-#endif
 
   /* Return it back */
   oledInvertDebugLink();
